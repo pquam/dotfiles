68cc99e452cdf34922c544709468f7e9164695a6
Patrick Quam <patrick@quam.computer>
2024-03-03 19:23:13 -0800

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,138 @@
+{ config, pkgs, ... }:
+
+{
+
+  imports = [
+    /etc/nixos/hardware-configuration.nix
+  ];
+
+  # paste your boot config here...
+boot.loader.systemd-boot.enable = true;
+boot.loader.efi.canTouchEfiVariables = true;
+
+  networking = {
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [];
+      allowedUDPPorts = [];
+    };
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
+
+  # edit as per your location and timezone
+  time.timeZone = "America/Los_Angles";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+      LC_CTYPE="en_US.UTF-8"; # required by dmenu don't change this
+    };
+  };
+
+  sound.enable = true;
+
+  services = {
+    xserver = {
+      layout = "us";
+      xkbVariant = "";
+      enable = true;
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          i3status
+        ];
+      };
+      desktopManager = {
+        xterm.enable = false;
+        xfce = {
+          enable = true;
+          noDesktop = true;
+          enableXfwm = false;
+        };
+      };
+      displayManager = {
+        lightdm.enable = true;
+        defaultSession = "xfce+i3";
+      };
+    };
+    gvfs.enable = true;
+    gnome.gnome-keyring.enable = true;
+    blueman.enable = true;
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+    };
+  };
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      pulseaudio = true;
+    };
+  };
+
+ 
+  users.users.patrickq = {
+    isNormalUser = true;
+    description = "Patrick Quam";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    git
+    gh
+    python3
+    vim
+    unzip
+    dmenu
+    google-chrome
+    vscode
+    discord
+  ];
+
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    google-chrome
+    vscode
+    discord
+];
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart =
+          "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+  
+  hardware = {
+    bluetooth.enable = true;
+  };
+
+  # Don't touch this
+  system.stateVersion = "23.11";
+}
